5d48f5eb84a1e4a0f13826d556e00cc2795b7816
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-28 13:59:17 +0700

    upload: app/dashboard/chat/page.tsx

---
+++ app/dashboard/chat/page.tsx
@@ -0,0 +1,10 @@
+import DashboardShell from "@/components/DashboardShell";
+import ChatClient from "@/components/ChatClient";
+
+export default function ChatPage() {
+  return (
+    <DashboardShell>
+      <ChatClient />
+    </DashboardShell>
+  );
+}
