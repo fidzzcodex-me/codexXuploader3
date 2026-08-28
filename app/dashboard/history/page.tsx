17df1b45fc717e3cfa84abbbf60eabf8d837a927
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-28 13:59:11 +0700

    upload: app/dashboard/history/page.tsx

---
+++ app/dashboard/history/page.tsx
@@ -0,0 +1,10 @@
+import DashboardShell from "@/components/DashboardShell";
+import HistoryClient from "@/components/HistoryClient";
+
+export default function HistoryPage() {
+  return (
+    <DashboardShell>
+      <HistoryClient />
+    </DashboardShell>
+  );
+}
