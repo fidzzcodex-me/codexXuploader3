c2c692c9f2751f8efce9e85fbd01c4bd6293f49c
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-28 13:59:14 +0700

    upload: app/dashboard/profile/page.tsx

---
+++ app/dashboard/profile/page.tsx
@@ -0,0 +1,10 @@
+import DashboardShell from "@/components/DashboardShell";
+import ProfileClient from "@/components/ProfileClient";
+
+export default function ProfilePage() {
+  return (
+    <DashboardShell>
+      <ProfileClient />
+    </DashboardShell>
+  );
+}
