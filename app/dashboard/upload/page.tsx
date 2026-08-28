547377862a796e1ef0e2197157fcab8fcee9069b
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-28 13:59:16 +0700

    upload: app/dashboard/upload/page.tsx

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,10 @@
+import DashboardShell from "@/components/DashboardShell";
+import UploadClient from "@/components/UploadClient";
+
+export default function UploadPage() {
+  return (
+    <DashboardShell>
+      <UploadClient />
+    </DashboardShell>
+  );
+}
